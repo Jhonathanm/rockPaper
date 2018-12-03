a73282e40b7737fc9e38916c8b091eb157050b46
JHONATHAN MEDINA <rl230016@rlas-macair-21.local>
2018-12-03 11:20:28 -0600

    Initial Commit

---
+++ rockPaper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rockPaper
+//
+//  Created by JHONATHAN MEDINA on 12/3/18.
+//  Copyright © 2018 JHONATHAN MEDINA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
